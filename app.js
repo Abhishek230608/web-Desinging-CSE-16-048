927b42e58792202163abf55d636a84e5730f194b
Abhishek230608 <abhishekpandey230608@gmail.com>
2026-04-06 19:57:02 +0530

    exp1

---
+++ app.js
@@ -0,0 +1,29 @@
+// let age=12;
+// const self="Aman";
+// const pi=3.14;
+// var name="Aman";
+// var Isemployed = false;
+// var name="Aditya";
+
+
+// let age=12;
+// const self="Aman";
+// const pi=3.14;
+// const Isemployed = false;
+// let Name="Aditya";
+// console.log(name);
+// console.log(age);
+// console.log(self);
+// console.log(pi);
+// console.log(Name);
+
+// if(true){
+//     var age=30;
+// }
+// console.log(age);
+
+
+if(true){
+    let age=30;
+}
+console.log(age);
