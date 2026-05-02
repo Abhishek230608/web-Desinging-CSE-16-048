762700c5f4792daf19dddc5e585fa8bdf91819da
Abhishek230608 <abhishekpandey230608@gmail.com>
2026-05-02 20:53:18 +0530

    exp

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,29 +1,57 @@
-// let age=12;
-// const self="Aman";
-// const pi=3.14;
-// var name="Aman";
-// var Isemployed = false;
-// var name="Aditya";
-
-
-// let age=12;
-// const self="Aman";
-// const pi=3.14;
-// const Isemployed = false;
-// let Name="Aditya";
-// console.log(name);
-// console.log(age);
-// console.log(self);
-// console.log(pi);
-// console.log(Name);
-
-// if(true){
-//     var age=30;
-// }
-// console.log(age);
-
-
-if(true){
-    let age=30;
+let person {
+    name: ""
+} ;
+console.log(typeof(person));
+let mixedArray=[1,"Hello",true,[1,2,3],{name:"Alice", age:25}];
+console.log(mixedArray[0]);
+function multiply(a,b){
+    return a*b;
 }
-console.log(age);
+console.log(multiply(2,3));
+//map
+let newarray=[1,2,3,4,5];
+let squaredArray= newarray.map((num) => num*5);
+console.log(squaredArray);
+//filter
+let evenNumbers=[1,2,3,4,5];
+let filteredEvenNumbers = evenNumbers.filter(num=> num%2===0);
+console.log(filteredEvenNumbers);
+//reduce
+let sum=[1,2,3,4,5];
+let total=sum.reduce((accumulator,currentValue)=> accumulator+currentValue,0);
+console.log(total);
+let sub=[1,2,3,4,5];
+let total=sub.reduce((accumulator,currentValue)=> accumulator+currentValue,2);
+console.log(total);
+//map
+let students=[
+    {name:"Alice", marks:85},
+        {name:"Bob",marks:92},
+         {name:"Charlie",marks:78}
+];
+let marks= students.map(student=>student.name);
+console.log(names);
+let topStudents =students.filter
+(student=>student.marks>80);
+console.log(topStudents);
+let totalMarks= students.reduce((acc,student)=>acc+student.marks,0);
+console.log(totalMarks);
+//mixed object
+let mixedObject={
+    name:"Alice",
+    age:25,
+    hobbies:{"reading","traveling","coding"},
+    Address:{
+        street:"123 Main St",
+        city:"New York",
+        country:"USA"
+    },
+    greet: function (){
+        console.log()
+    }
+}
+// array inside object 
+let company = {
+    name: "tech corp"
+    
+}
